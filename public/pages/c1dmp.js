f103fae53c1f0d8a286ae0f5e73ee5f664aacc97
Shahar Zur <shaharz@wix.com>
2017-02-12 20:57:00 +0200

    hack17 changes

--- public/pages/c1dmp.js
+++ public/pages/c1dmp.js
@@ -1,7 +1,7 @@
 $w.onReady(function () {
-//fdgfdgfg
+	//TODO: write your page related code here...
 });
 
 export function container1_onClick(event) {
-	//Add your code for this event here: 
+	//changes in hackathon17
 }

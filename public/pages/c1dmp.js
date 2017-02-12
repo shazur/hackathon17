21b8c2f95074cbfc2d3880e4e22a87135f4a3410
Shahar Zur <shaharz@wix.com>
2017-02-12 17:50:01 +0200

    bla bla

--- public/pages/c1dmp.js
+++ public/pages/c1dmp.js
@@ -1,7 +1,7 @@
-08e3c912a22b08e83ff8c4937b111982f940d0c1$w.onReady(function () {
-	//TODO: write your page related code here...
+$w.onReady(function () {
+	//PUSHINGGGGG!!!!
 });
 
 export function container1_onClick(event) {
-	$w('#container1').hide();
+	//Add your code for this event here: 
 }
